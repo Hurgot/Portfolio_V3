ccf31923cadb9e02f1f55585379070b0c71629c9
Wattson <boudet.hugo.sio@gmail.com>
2026-04-01 16:10:33 +0200

    Fix: Remplacer les images locales par des placeholders pour éviter la disparition lors du push
    - Ajout d'un .gitignore pour ignorer les images volumineuses
    - Création d'un .gitkeep pour garder le dossier images
    - Modification des URLs d'images pour utiliser Lorem Picsum

--- js/script.js
+++ js/script.js
@@ -80,19 +80,19 @@ document.addEventListener('DOMContentLoaded', function() {
             {
                 "title": "Site Web Personnel",
                 "description": "Un portfolio simple en HTML/CSS/JS pour présenter mes travaux.",
-                "image": "images/portfolio.jpg",
+                "image": "https://picsum.photos/400/300?random=1",
                 "link": "#"
             },
             {
                 "title": "Framework Laravel",
                 "description": "Un projet de développement d'une application web avec Laravel.",
-                //"image": "images/laravel.jpg",
+                "image": "https://picsum.photos/400/300?random=2",
                 "link": "#"
             },
             {
                 "title": "Certification RGPD",
                 "description": "Certification réalisée lors de ma première année de BTS",
-                //"image": "images/nom-de-votre-image.jpg",
+                "image": "https://picsum.photos/400/300?random=3",
                 "link": "#"
             }
         ],
